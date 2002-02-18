ced9c4ad2a4e856df3d24efed5e64edbe46b673f
rherveille <rherveille@998c4a30-3bb8-4a4a-a719-f571b6601ac9>
2002-02-18 14:29:39 +0000

    renamed 'atahost.vhd' to 'atahost_top.vhd' renamed 'controller.vhd' to 'atahost_controller.vhd' renamed 'pio_tctrl.vhd' to 'atahost_pio_tctrl.vhd' broke-up 'counter.vhd' into 'ud_cnt.vhd' and 'ro_cnt.vhd' changed resD input to generic RESD in ud_cnt.vhd changed ID input to generic ID in ro_cnt.vhd changed core to reflect changes in ro_cnt.vhd removed references to 'count' library changed IO names added disclaimer added CVS log moved registers and wishbone signals into 'atahost_wb_slave.vhd' core is now equivalent to verilog version

--- rtl/vhdl/ocidec1/atahost_controller.vhd
+++ rtl/vhdl/ocidec1/atahost_controller.vhd
@@ -1,13 +1,59 @@
---
--- file: controller.vhd
---	description: OCIDEC1 OpenCores IDE controller type-1
--- author : Richard Herveille
+---------------------------------------------------------------------
+----                                                             ----
+----  OpenCores IDE Controller (OCIDEC-1)                        ----
+----  PIO Contoller                                              ----
+----                                                             ----
+----  Author: Richard Herveille                                  ----
+----          richard@asics.ws                                   ----
+----          www.asics.ws                                       ----
+----                                                             ----
+---------------------------------------------------------------------
+----                                                             ----
+---- Copyright (C) 2001, 2002 Richard Herveille                  ----
+----                          richard@asics.ws                   ----
+----                                                             ----
+---- This source file may be used and distributed without        ----
+---- restriction provided that this copyright statement is not   ----
+---- removed from the file and that any derivative work contains ----
+---- the original copyright notice and the associated disclaimer.----
+----                                                             ----
+----     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ----
+---- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ----
+---- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ----
+---- FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ----
+---- OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ----
+---- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ----
+---- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ----
+---- GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ----
+---- BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ----
+---- LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ----
+---- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ----
+---- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ----
+---- POSSIBILITY OF SUCH DAMAGE.                                 ----
+----                                                             ----
+---------------------------------------------------------------------
+
 -- rev.: 1.0  march 18th, 2001
 -- rev.: 1.0a april 12th, 2001. Removed references to records.vhd to make it compatible with freely available VHDL to Verilog converter tools
 -- rev.: 1.1  june  18th, 2001. Changed PIOack generation. Avoid asserting PIOack continuously when IDEen = '0'
 -- rev.: 1.2  june  26th, 2001. Changed dPIOreq generation. Core did not support wishbone burst accesses to ATA-device.
 -- rev.: 1.3  july  11th, 2001. Changed PIOreq & PIOack generation (made them synchronous). 
 --
+--
+--  CVS Log
+--
+--  $Id: atahost_controller.vhd,v 1.1 2002-02-18 14:29:39 rherveille Exp $
+--
+--  $Date: 2002-02-18 14:29:39 $
+--  $Revision: 1.1 $
+--  $Author: rherveille $
+--  $Locker:  $
+--  $State: Exp $
+--
+-- Change History:
+--               $Log: not supported by cvs2svn $
+--
+
 
 -- OCIDEC1 supports:	
 -- -Common Compatible timing access to all connected devices
@@ -17,7 +63,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 
-entity controller is
+entity atahost_controller is
 	generic(
 		TWIDTH : natural := 8;                        -- counter width
 
@@ -28,8 +74,8 @@ entity controller is
 		PIO_mode0_Teoc : natural := 23                -- 240ns ==> T0 - T1 - T2 = 600 - 70 - 290 = 240
 	);
 	port(
-		clk : in std_logic;  		                    	  -- master clock in
-		nReset	: in std_logic := '1';                 -- asynchronous active low reset
+		clk : in std_logic;                           -- master clock in
+		nReset : in std_logic := '1';                 -- asynchronous active low reset
 		rst : in std_logic := '0';                    -- synchronous active high reset
 		
 		irq : out std_logic;                          -- interrupt request signal
@@ -53,26 +99,26 @@ entity controller is
 		PIOwe  : in std_logic;                        -- PIO direction bit '1'=write, '0'=read
 
 		-- ATA signals
-		RESETn	: out std_logic;
+		RESETn : out std_logic;
 		DDi  	 : in std_logic_vector(15 downto 0);
 		DDo    : out std_logic_vector(15 downto 0);
 		DDoe   : out std_logic;
-		DA    	: out unsigned(2 downto 0);
-		CS0n	  : out std_logic;
-		CS1n	  : out std_logic;
+		DA     : out unsigned(2 downto 0);
+		CS0n   : out std_logic;
+		CS1n   : out std_logic;
 
 		DIORn	: out std_logic;
 		DIOWn	: out std_logic;
 		IORDY	: in std_logic;
 		INTRQ	: in std_logic
 	);
-end entity controller;
+end entity atahost_controller;
 
-architecture structural of controller is
+architecture structural of atahost_controller is
 	--
 	-- Component declarations
 	--
-	component PIO_tctrl is	
+	component atahost_pio_tctrl is	
 	generic(
 		TWIDTH : natural := 8;                   -- counter width
 
@@ -108,7 +154,7 @@ architecture structural of controller is
 		DIOW : buffer std_logic;                 -- IOwrite signal, active high
 		IORDY : in std_logic                     -- IORDY signal
 	);
-	end component PIO_tctrl;
+	end component atahost_pio_tctrl;
 
 	--
 	-- signals
@@ -159,33 +205,33 @@ begin
 		begin
 			if (nReset = '0') then
 				RESETn <= '0';
-				DIORn <= '1';
-				DIOWn <= '1';
-				DA <= (others => '0');
-				CS0n	<= '1';
-				CS1n	<= '1';
-				DDo <= (others => '0');
-				DDoe <= '0';
+				DIORn  <= '1';
+				DIOWn  <= '1';
+				DA     <= (others => '0');
+				CS0n  	<= '1';
+				CS1n	  <= '1';
+				DDo    <= (others => '0');
+				DDoe   <= '0';
 			elsif (clk'event and clk = '1') then
 				if (rst = '1') then
 					RESETn <= '0';
-					DIORn <= '1';
-					DIOWn <= '1';
-					DA <= (others => '0');
-					CS0n	<= '1';
-					CS1n	<= '1';
-					DDo <= (others => '0');
-					DDoe <= '0';
+					DIORn  <= '1';
+					DIOWn  <= '1';
+					DA     <= (others => '0');
+					CS0n	  <= '1';
+					CS1n	  <= '1';
+					DDo    <= (others => '0');
+					DDoe   <= '0';
 				else
 					RESETn <= not IDEctrl_rst;
-					DA <= PIOa(2 downto 0);
-					CS0n	<= not (not PIOa(3) and PIOreq); -- CS0 asserted when A(3) = '0'
-					CS1n	<= not (    PIOa(3) and PIOreq); -- CS1 asserted when A(3) = '1'
+					DA     <= PIOa(2 downto 0);
+					CS0n	  <= not (not PIOa(3) and PIOreq); -- CS0 asserted when A(3) = '0'
+					CS1n	  <= not (    PIOa(3) and PIOreq); -- CS1 asserted when A(3) = '1'
 
-					DDo <= PIOd;
-					DDoe <= PIOoe;
-					DIORn <= not PIOdior;
-					DIOWn <= not PIOdiow;
+					DDo    <= PIOd;
+					DDoe   <= PIOoe;
+					DIORn  <= not PIOdior;
+					DIOWn  <= not PIOdiow;
 				end if;
 			end if;
 		end process gen_regs;
@@ -212,7 +258,7 @@ begin
 	begin
 		if (clk'event and clk = '1') then
 			dPIOreq <= PIOreq and not PIOack;
-			PIOgo <= (PIOreq and not dPIOreq) and IDEctrl_IDEen;
+			PIOgo   <= (PIOreq and not dPIOreq) and IDEctrl_IDEen;
 		end if;
 	end process gen_PIOgo;
 
@@ -226,11 +272,32 @@ begin
 	--
 	-- hookup timing controller
 	--
-	PIO_timing_controller: PIO_tctrl
-		generic map (TWIDTH => TWIDTH,
-			PIO_mode0_T1 => PIO_mode0_T1, PIO_mode0_T2 => PIO_mode0_T2, PIO_mode0_T4 => PIO_mode0_T4, PIO_mode0_Teoc => PIO_mode0_Teoc)
-		port map (clk => clk, nReset => nReset, rst => rst, IORDY_en => IORDYen, T1 => T1, T2 => T2, T4 => T4, Teoc => Teoc, 
-			go => PIOgo, we => PIOwe, oe => PIOoe, done => PIOdone, dstrb => dstrb, DIOR => PIOdior, DIOW => PIOdiow, IORDY => sIORDY);
+	PIO_timing_controller: atahost_pio_tctrl
+		generic map (
+			TWIDTH         => TWIDTH,
+			PIO_mode0_T1   => PIO_mode0_T1,
+			PIO_mode0_T2   => PIO_mode0_T2,
+			PIO_mode0_T4   => PIO_mode0_T4,
+			PIO_mode0_Teoc => PIO_mode0_Teoc
+		)
+		port map (
+			clk      => clk,
+			nReset   => nReset,
+			rst      => rst,
+			IORDY_en => IORDYen,
+			T1       => T1,
+			T2       => T2,
+			T4       => T4,
+			Teoc     => Teoc, 
+			go       => PIOgo,
+			we       => PIOwe,
+			oe       => PIOoe,
+			done     => PIOdone,
+			dstrb    => dstrb,
+			DIOR     => PIOdior,
+			DIOW     => PIOdiow,
+			IORDY    => sIORDY
+		);
 
 	-- generate acknowledge
 	gen_ack: process(clk)
@@ -240,3 +307,4 @@ begin
 		end if;
 	end process gen_ack;
 end architecture structural;
+
